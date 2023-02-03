a2c95a68a11e1b10805922706d430d85c02bb0e8
Andrii Grechyn <gre4ya@icloud.com>
2023-02-02 20:27:00 -0600

    created Test Case 1

---
+++ src/test/java/test_data/TestData.java
@@ -0,0 +1,9 @@
+package test_data;
+
+public class test_data {
+
+    public static final String homePageTitle = "Carvana | Buy & Finance Used Cars Online | At Home Delivery";
+    public static final String homePageURL = "https://www.carvana.com/";
+
+
+}
