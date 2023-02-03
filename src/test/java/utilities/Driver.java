c3bb6134fbbc67da84eec1d6f184fe2e80ea19ad
Andrii Grechyn <gre4ya@icloud.com>
2023-02-02 19:42:39 -0600

    created utilities pacage

---
+++ src/test/java/utilities/Driver.java
@@ -0,0 +1,19 @@
+package utilities;
+
+import org.openqa.selenium.WebDriver;
+
+public class Driver {
+
+    private static WebDriver driver;
+
+    private Driver(){}
+
+    public static WebDriver getDriver() {
+        if(driver != null){
+            switch (driver){
+
+            }
+        }
+        return driver;
+    }
+}
